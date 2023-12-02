e57b9dffacd6492a2ddaa58bcbba7c1b8e1d519e
batsyboy <98476812+sichaholmes2@users.noreply.github.com>
2023-12-02 18:44:18 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,67 @@
+//sinngleton object
+const tinderUser= new Object()
+
+tinderUser.id="123abc"
+tinderUser.name="Sam"
+tinderUser.isLoggedIn = false
+console.log(tinderUser)
+
+const regularUser={
+    email:"some@gamil.com",
+    //objects inside an object
+    fullname:{
+        //another object inside
+        userfullname:{
+        firstname:"hitesh",
+        lastname:"choudhary"
+        }
+
+    }
+}
+
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname.firstname);
+//optional chaining if any one doesnt exist is use ? or use if else
+
+//number string as key value pair
+const obj1 = {1: "a" , 2 :"b"}
+const obj2 = {3: "a" , 4 :"b"}
+
+const obj3={obj1, obj2}
+console.log(obj3)//obj3 is object of objects
+
+
+const obj4=Object.assign({}, obj1 ,obj2)//merges it properly, {} is optional
+console.log(obj4)
+
+//spread operator to do the same
+const obj5={...obj1, ...obj2}
+console.log(obj5)
+
+//when u get an array of multiple objects form a database from a databasze
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+
+//to display inly the keys
+console.log(Object.keys(tinderUser));
+
+//to display inly the values
+console.log(Object.values(tinderUser));
+
+//entries
+console.log(Object.entries(tinderUser));
