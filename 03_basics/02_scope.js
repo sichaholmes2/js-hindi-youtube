9cf4dd008354904dd581f61c95c543a71835301d
batsyboy <98476812+sichaholmes2@users.noreply.github.com>
2023-12-03 20:05:31 +0000

    scope2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,10 +10,49 @@ if(true){
     var c =30
 }
 
-for(let i= 0; i<array.length;++i){
-    const element = array[i];
-}
+// for(let i= 0; i<array.length;++i){
+//     const element = array[i];
+// }
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)//prints 30
+
+//function inside a function
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website ="youtube"
+        console.log(username)//two can get username from one
+    }
+
+    //console.log(website);// this will say website is not defoned
+    two()
+}
+//one()
+
+if(true){
+    const username="hitesh"
+    if(username === "hitesh"){
+        const website= " youtube"
+        console.log(username+website)
+    }//console.log(website)// you cant access website
+}
+
+//console.log(username)//username is also out of scope
+
+// ++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+return num + 1
+}
+
+
+addTwo(5)//error as add Two is stored in a variable wgule addOne is not so it can be used before declaration(hoisting)
+const addTwo = function(num){
+    return num+2
+}
+
+ 
